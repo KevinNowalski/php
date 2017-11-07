3288ad66862972528f899b530992b9a0f3003621
Kevin Nowalski <kevinnowalski@yahoo.com>
2017-11-07 13:46:03 -0500

    Create config.php

---
+++ loginSystem/config.php
@@ -0,0 +1,13 @@
+<?php
+$dbuser = "root";
+$dbPass = "";
+$dbDatabase = "test";
+$dbHost = "localhost";
+$dbConn = mysqli_connect($dbHost, $dbuser, $dbPass);
+if ($dbConn){
+    mysqli_select_db($dbConn, $dbDatabase);
+    print("<strong>Successfully connected to the database!</strong></br>");
+}else{
+    die("<strong>Error</strong> Could not connect to the database.");
+}
+?>
