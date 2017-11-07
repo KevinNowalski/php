952b3c731e7d9e260ceabd6d9cc3198e7cf7c17f
Kevin Nowalski <kevinnowalski@yahoo.com>
2017-11-07 13:47:32 -0500

    Create login.php

---
+++ loginSystem/login.php
@@ -0,0 +1,25 @@
+<?php
+require ('config.php');
+if(isset($_POST['submitbutton'])){
+    if(!isset($_POST['email'])){
+        die("Error.  The email field was not set.");
+    }else if(!isset($_POST['password'])){
+            die("Error.  The password field was not set.");
+        }
+    }
+    
+    $password = hash("sha512", $_POST['password']);
+    $query = mysqli_query($dbConn, "SELECT id FROM users WHERE email = ".mysqli_real_escape_string($dbConn, $_POST['email'])."AND WHERE password = ".mysqli_real_escape_string($dbConn, $password));
+    //$query = mysqli_query($dbConn, "SELECT id FROM 'users' WHERE email = " .$_POST['email']);
+    //$query = mysqli_query($dbConn, "SELECT * FROM users");
+    //print "Your query is ".$query;
+    //while($row = mysqli_fetch_assoc($query)){
+               // print($row['username']."</br>");
+           // }
+    //print"Your email is " .$_POST['email']. " and your password is ".$password."</br>";
+    if (mysqli_num_rows($query)){
+        die("Login credentials correct.");
+    }else {
+        die("Error. Incorrect password or email address");
+    }
+?>
